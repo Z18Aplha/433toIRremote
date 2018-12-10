6266cc087ddfb0c1f8fd52558080aff09ec0efc0
Aaron <aaron.winkler98@googlemail.com>
2018-12-10 19:02:43 +0100

    v1.1

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,11 @@ RCSwitch myReceiver = RCSwitch();
 IRsend irTransmitter; //irLed must be connected to PWM PIN 3
 
 //receiving variables
-int prev_signal = 0;
+unsigned long prev_signal = 0;
 unsigned long timestamp = 0;
+unsigned long adress = 117440512;  //0x7000000
+unsigned long onkyo_internal_offset = 1258291200;
+unsigned long LED_internal_offset = 16711680;
 
 
 
@@ -26,24 +29,94 @@ void loop() {
 
   if (myReceiver.available()) {
 
-    int signal = myReceiver.getReceivedValue();
-    char signal_hex[] = "";
+    unsigned long signal = myReceiver.getReceivedValue();
+    unsigned long code = signal - adress;
+    bool send = true;
 
 
     Serial.println("");
     Serial.print("message received: ");
     Serial.println(signal);
+    Serial.print("code extracted: ");
+    Serial.println(code);
 
-    if ((!(signal == prev_signal) || millis() - timestamp > 200) && (signal > 0) && !(signal == 23040)){  //23040 indicates issues (dont know why), need to be filtered! transformation in transmitter!
+    String code_hex = String(code, HEX);
+
+
+    if (signal == prev_signal) {
+      Serial.print("multiple received signal");
+      if (millis() - timestamp < 500) {
+        Serial.println(" - not send");
+        send = false;
+      }
+      else {
+        Serial.println(" - but enough time between them");
+      }
+    }
+
+    if (signal == 23040) {
+      Serial.println("received 23040, frequently send error - not send");
+      send = false;
+    }
+
+    if (signal == 0) {
+      Serial.println("no signal - not send");
+      send = false;
+    }
+
+    if (code == 1261830144) {
+      Serial.println("frequently error signal - not send");
+      send = false;
+    }
+
+    if (((code - LED_internal_offset == 0) || (code - onkyo_internal_offset == 0))) {
+      Serial.println("signal lost - not send");
+      send = false;
+    }
+
+    if (!(code_hex.indexOf("4b") == 0) || (code_hex.indexOf("ff") == 0)) {
+      Serial.println("signal is not a type of Onky or LED - not send");
+      send = false;
+    }
+
+    if (send) {
+      timestamp = millis();
+      prev_signal = signal;
+
       irTransmitter.sendNEC(signal, 32);
-      itoa(signal, signal_hex, 16);
 
       Serial.print("NEC send: ");
-      Serial.println(signal_hex);
+      Serial.println(code_hex);
+
+    }
+    else {
+      send = true;
+    }
+
+
+
+
+
+
+/*
+
+    if ((!(signal == prev_signal) || millis() - timestamp > 200) && !((code - LED_internal_offset == 0) || (code - onkyo_internal_offset == 0)) && (code > 0) && !(code == 1261830144) && ((code_hex.indexOf("4b") == 0) || (code_hex.indexOf("ff") == 0)) && !(signal == 23040)){  //23040 indicates issues (dont know why), need to be filtered! transformation in transmitter!
+      timestamp = millis();
+      prev_signal = signal;
+      Serial.println("inloop");
+      irTransmitter.sendNEC(signal, 32);
+      Serial.println("send done");
+
+      Serial.print("NEC send: ");
+      Serial.println(code_hex);
     }
     else{
+      timestamp = millis();
+      prev_signal = signal;
       Serial.println("received multiple times, wrong format or '40'");
     }
+    */
+
+    myReceiver.resetAvailable();
   }
-  myReceiver.resetAvailable();
 }
